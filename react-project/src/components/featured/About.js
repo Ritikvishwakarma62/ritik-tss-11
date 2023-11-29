53d1a514411e8aa3beeb524bb413e7714cda5e28
Ritikvishwakarma62 <rv4559751@gmail.com>
2023-11-29 11:14:16 -0800

    this will add new code

--- react-project/src/components/featured/About.js
+++ react-project/src/components/featured/About.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const About = () => {
+  let [age,setAge] = useState(20)
+  let demo = ()=>{
+    setAge(10)
+      
+  }
   return (
-    <div>About</div>
+    <div>
+      <h1>{age} </h1>
+      <button className='btn btn-warning' onClick={demo} >OK</button>
+    </div>
   )
 }
 
